f9d54270109dbac58ce0fc8e95fe12bd3dd9c265
姚帅东 <2986697278@qq.com>
2021-11-09 16:38:41 +0800

    初次运行成功

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("query/{id}")
+    public String GetUser(@PathVariable int id){
+        return userService.Sel(id).toString();
+    }
+}
